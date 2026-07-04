ff3d74423b63a8508956a9c1878dbbd131173a14
Ananya Sharma <ananyasharma@gmail.com>
2026-07-04 11:31:39 +0530

    REDIS CACHING DONE

--- level2/phase1/index.js
+++ level2/phase1/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./lib/db.js"; // Import the connectDB function
 import User from "./model/user.model.js"; // Import the User model
+import Redis from "ioredis"; // Import the Redis client
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 //middleware
 app.use(express.json());
+
+// redis connection
+const redis = new Redis(process.env.REDIS_URL);
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -16,7 +21,21 @@ app.get("/", (req, res) => {
 //create krne ke liye api
 app.post("/create", async (req, res) => {
   const { name, email, password } = req.body;
+  await redis.del("user:all"); // Clear the cached data in Redis when a new user is created
   const user = await User.create({name, email, password,});
+  return res.json(user);
+});
+
+// redis get api
+app.get("/get-with-redis", async (req, res) => {
+  // Check if the data is already cached in Redis
+  const cached = await redis.get("user:all");
+  if (cached) {
+    return res.json(JSON.parse(cached));
+  }
+  // If not cached, fetch from the database and cache it in Redis
+  const user = await User.find({});
+  await redis.set("user:all", JSON.stringify(user));
   return res.json(user);
 });
 
@@ -31,6 +50,8 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
 
-
+//redis m string format m store krte hai data ko
+//redis ko get krne ke liye use krte hai 
 
 //without redis get krne m 64ms lage 
+//with redis get krne m 18ms lage
